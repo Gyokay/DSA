cfedb2715d91d89e08519396bf2bed11be1669a5
Gyokay Ahmed <gyokayakmed@gmail.com>
2017-01-13 15:44:40 +0200

    Add stack implementation

---
+++ C#/DSA/Stack/Contracts/IStack.cs
@@ -0,0 +1,11 @@
+﻿namespace Stack.Contracts
+{
+    public interface IStack<T>
+    {
+        void Push(T data);
+
+        T Pop();
+
+        T Peek();
+    }
+}
---
+++ C#/DSA/Stack/Stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Stack.Contracts;
+
+namespace Stack
+{
+    public class Stack<T> : IStack<T>
+    {
+        private T[] stack;
+        private int topIndex;
+
+        public Stack(int capacity)
+        {
+            this.stack = new T[capacity];
+            this.topIndex = 0;
+        }
+
+        public Stack(int capacity, T firstElement) : this(capacity)
+        {
+            this.stack[topIndex++] = firstElement;
+        }
+
+        public int Count
+        {
+            get { return topIndex; }
+        }
+
+        public void Push(T data)
+        {
+            if (this.isFull())
+            {
+                throw new InvalidOperationException("Stack is full");
+            }
+
+            this.stack[topIndex++] = data;
+        }
+
+        public T Pop()
+        {
+            if (isEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return this.stack[--topIndex];
+        }
+
+        public T Peek()
+        {
+            if (this.isEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return this.stack[this.topIndex - 1];
+        }
+
+        private bool isFull()
+        {
+            return this.stack.Length == this.topIndex;
+        }
+
+        private bool isEmpty()
+        {
+            return topIndex == 0;
+        }
+    }
+}
